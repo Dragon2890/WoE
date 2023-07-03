8564d0a4ce6a8c8bfaeb59b4606a2fdbdf9258d8
Daragh <djw2890@hotmail.com>
2023-07-03 13:54:00 +0100

    Day 1

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'WE',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  
+  inputFieldString: string=""
 
-  constructor() {}
+  listInput: string[]=[]
 
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+
+  }
+
+  NextPage() {
+    this.router.navigate(['/page2'])
+  }
+
+  AddList() {
+    this.listInput.push(this.inputFieldString)
+    this.listInput.forEach(inputFieldString => {
+    console.log(inputFieldString)
+    });
+  }
 }
